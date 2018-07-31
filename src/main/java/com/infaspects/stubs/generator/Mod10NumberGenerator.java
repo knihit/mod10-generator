fd1befa54e6fe008de9017b9cd4d4d8fdda6ea4a
knihit <knihit@yahoo.com>
2018-07-30 22:45:45 -0400

    updated files

--- src/main/java/com/infaspects/stubs/generator/Mod10NumberGenerator.java
+++ src/main/java/com/infaspects/stubs/generator/Mod10NumberGenerator.java
@@ -5,21 +5,35 @@ import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+/**
+ * Generate a random number of a given length with the check digit that passes mod10 test
+ */
 @Component
 public class Mod10NumberGenerator {
 
     private int numberLength;
 
+    /**
+     * Constructor to initialize the object with the length of the random number to be generated
+     *
+     * @param numberLength - lenght of the random number including the check digit
+     *
+     */
     public Mod10NumberGenerator(int numberLength) {
         this.numberLength = numberLength;
     }
 
-    public String getMod10Number(int numberLength) {
+    /**
+     * Generate the random number with the check digit
+     *
+     * @return random number with the check digit
+     */
+    public String getMod10Number() {
         Random random = new Random(System.currentTimeMillis());
         StringBuilder mod10String = new StringBuilder();
 
 
-        for (int index = 0; index < numberLength - 1; ++index) {
+        for (int index = 0; index < this.numberLength - 1; ++index) {
             mod10String.append(random.nextInt(10));
         }
 
---
+++ src/test/java/com/infaspects/stubs/AppTest.java
@@ -0,0 +1,47 @@
+package com.infaspects.stubs;
+
+import com.infaspects.stubs.generator.CardNumberGenerator;
+import jdk.internal.jline.internal.TestAccessible;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Autowired
+    Mod10NumberGenerator mod10NumberGeneratorTest;
+
+    @Autowired
+    CardNumberGenerator cardNumberGeneratorTest;
+
+    //@Bean
+    //public Mod10NumberGenerator getMod10NumberGeneratorTest() {
+    //    return new Mod10NumberGenerator(9);
+    //}
+
+    @Bean
+    public CardNumberGenerator getCardNumberGeneratorTest() {
+        return new CardNumberGenerator("4519", "02", mod10NumberGeneratorTest.getMod10Number());
+    }
+
+    /*@Test
+    public void testMod10Generator() {
+        String number = mod10NumberGeneratorTest.getMod10Number();
+        System.out.println(number);
+        if (number.length() == 9) {
+            assert true;
+        } else {
+            assert false;
+        }
+    }*/
+
+    @Test
+    public void testCardNumberGenerator() {
+        String cardNumber = cardNumberGeneratorTest.generateClientCard();
+        System.out.println(cardNumber);
+
+        assertTrue(cardNumber.length() == 16);
+    }
+}
+
