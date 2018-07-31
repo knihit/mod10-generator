584605b256562784a3aad3865cbaaa4e9a75cf5b
knihit <knihit@yahoo.com>
2018-07-30 23:55:38 -0400

    unit test updated

--- src/test/java/com/infaspects/stubs/AppTest.java
+++ src/test/java/com/infaspects/stubs/AppTest.java
@@ -1,27 +1,39 @@
 package com.infaspects.stubs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.infaspects.stubs.generator.CardNumberGenerator;
 import com.infaspects.stubs.generator.Mod10NumberGenerator;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import javax.annotation.Resource;
+
 import static org.junit.Assert.assertTrue;
 
+@ContextConfiguration(
+        classes={Mod10NumberGenerator.class, CardNumberGenerator.class}
+)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class AppTest {
 
-    @Autowired
+    @Resource(name="mod10NumberGeneratorTest")
     Mod10NumberGenerator mod10NumberGeneratorTest;
 
-    @Autowired
+    @Resource(name="cardNumberGeneratorTest")
     CardNumberGenerator cardNumberGeneratorTest;
 
-    @Bean
-    public Mod10NumberGenerator getMod10NumberGeneratorTest() {
+    @Bean(name="mod10NumberGeneratorTest")
+    public Mod10NumberGenerator mod10NumberGeneratorTest() {
         return new Mod10NumberGenerator(9);
     }
 
-    @Bean
-    public CardNumberGenerator getCardNumberGeneratorTest() {
+    @Bean(name="cardNumberGeneratorTest")
+    public CardNumberGenerator cardNumberGeneratorTest() {
         return new CardNumberGenerator("4519", "02", mod10NumberGeneratorTest.getMod10Number());
     }
 
